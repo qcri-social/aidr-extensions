c6bfae633a4f00c1d362497c5daa8bee5a244296
jikimlucas <jikimlucas@me.com>
2014-09-11 13:35:05 +0300

    init. checkin

---
+++ aidr-gis/src/main/java/qa/qcri/aidr/gis/store/LookUp.java
@@ -0,0 +1,28 @@
+package qa.qcri.aidr.gis.store;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jlucas
+ * Date: 6/3/14
+ * Time: 12:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LookUp {
+
+
+
+    public static String MAP_GEOJSON_URL ="";
+    public static String DEFAULT_ESRI_GEO_FILE_PATH = "/var/www/data/esri/";
+    //public static String DEFAULT_ESRI_GEO_FILE_PATH = "";
+    public static String DEFAULT_ESRI_STORY_MAP_FILE_NAME = "esri";
+    public static String DEFAULT_ESRI_STORY_MAP_FILE_EXTENSION = ".geojson";
+
+    public static String DEFAULT_ESRI_DATA = "";
+
+    public static String MANAGER_URL = "http://localhost:8080/AIDRFetchManager";
+    public static String MANAGER_PUBLIC_COLLECTION_SERVICE = "/public/collection/";
+
+    public static int BOUNDING_BOX_ELEMENT_SIZE = 4;
+
+    public static String COMMA_SEPERATEOR = ",";
+}
