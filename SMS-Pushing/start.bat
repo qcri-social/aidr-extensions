2cc7aa5da00681551614da3b51f106b419075118
jikimlucas <jikimlucas@me.com>
2015-09-07 23:38:57 +0300

    aidr sms client call example

---
+++ SMS-Pushing/start.bat
@@ -0,0 +1,1 @@
+%JAVA_HOME%/bin/java -jar target/SMS-Pushing-0.0.1-jar-with-dependencies.jar  -url=%1 -collection-code=%2 -file=%3
---
+++ SMS-Pushing/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+$JAVA_HOME/bin/java -jar target/SMS-Pushing-0.0.1-jar-with-dependencies.jar  -url=$1 -collection-code=$2 -file=$3
